acf9693dd4593db2f872af65defc43c76532540f
PaulineDegreffe <degreffe.pauline@gmail.com>
2021-05-19 21:44:34 +0200

    conception de la vitrine

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <img src="./src/img/bitly-logo.png" alt="logoBilty">
+        </img>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Why Bilty ?</a>
+                    <a href="#">Solution</a>
+                    <a href="#">Features</a>
+                    <a href="#">Pricing</a>
+                    <a href="#">Ressource</a>
+                </li>
+            </ul>
+        </nav>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Login</a>
+                    <a href="#">Sign up</a>
+                    <a href="#">Get a Quote</a>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <div>
+                <h1>Short links, big results</h1>
+                <p class="titletextlight">A URL shortener built with powerful tools to help you grow and protect your brand.</p>
+                <a href="#">Get Started for Free</a>
+            </div>
+            <img src="./src/img/background-mobile.jpg" alt="background">
+        </section>
+
+        <section>
+            <form action="">
+                <input type="url">
+                <text>Shorten your link</text>
+                </input>
+                <input type="submit">
+
+                </input>
+            </form>
+
+        </section>
+        <p>By clicking SHORTEN, you are agreeing to Bitly’s <a href="">Terms of Service</a> and <a href="">Privacy Policy</a></p>
+        <section>
+            <h2>The most recognized brands in the world love Bitly</h2>
+            <div>
+                <img src="./src/img/Logo01.svg" alt="">
+                <img src="./src/img/Logo02.svg" alt="">
+                <img src="./src/img/Logo03.svg" alt="">
+                <img src="./src/img/Logo04.svg" alt="">
+                <img src="./src/img/Logo05.svg" alt="">
+                <img src="./src/img/Logo06.svg" alt=""><img src="" alt="">
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <img src="./src/img/bitly-logo.png" alt="logoBilty"" alt="">
+        <p> 2021 &copy; Bitly </p>
+        </img>
+    </footer>
+</body>
+
+</html>
